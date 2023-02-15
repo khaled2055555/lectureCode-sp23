4ae434139cca68f7733171f5513a20e0e8860ee3
khaled2055555 <114448530+khaled2055555@users.noreply.github.com>
2023-02-14 19:18:38 -0800

    Update AList.java

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -4,32 +4,43 @@ package lec7_lists4.DIY;
  *  in case you want to try to figure out how to write it yourself.
  *  After writing your methods, you can run the AListTest file.
  */
-public class AList {
+public class AList<T> {
+    private int size;
+    private T[] lst;
     /** Creates an empty list. */
     public AList() {
+        lst = (T[]) new Object[100];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
-    public void addLast(int x) {
+    public void addLast(T x) {
+        lst[size] = x;
+        size += 1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return lst[size - 1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        
+        return lst[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        T removed = lst[size - 1];
+        size -= 1;
+        lst[size - 1] = null;
+        
+        return removed;
     }
 } 
